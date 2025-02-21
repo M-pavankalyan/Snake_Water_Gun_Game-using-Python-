cc14d16d6a9d711b9fabfefbbf61c6c1eba28489
M-pavankalyan <166612329+M-pavankalyan@users.noreply.github.com>
2025-02-21 20:17:14 +0530

    Add files via upload

---
+++ snake_water_gun.py
@@ -0,0 +1,23 @@
+import random
+print("key:- s:snake w:water g:gun")
+computer = random.choice([1,-1,0])
+youstr = input("chosse your key:")
+youdisc = {"s":1,"w":-1,"g":0}
+reverse_disc = {1:"snake",-1:"water",0:"gun"}
+you = youdisc[youstr]
+print(f"\n you choosed {reverse_disc[you]} \n computer chossed {reverse_disc[computer]}")
+if(you == computer):
+    print("Draw Match!")
+else:
+    if(computer == 1 and you == 0):
+        print("\n You Won!")
+    elif(computer == -1 and you == 1):
+        print("\n You Won!")
+    elif(computer == 0 and you == -1):
+        print("\n You Won!")
+    elif(computer == 1 and you == -1):
+        print("\n You lose!")
+    elif(computer == -1 and you == 0):
+        print("\n You lose!")
+    elif(computer == 0 and you == -1):
+        print("\n You lose!")
