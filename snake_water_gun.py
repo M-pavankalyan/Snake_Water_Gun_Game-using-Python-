0507389c9c4e9f2b4b31790a4b8b3a65bd3c46bc
M-pavankalyan <166612329+M-pavankalyan@users.noreply.github.com>
2025-02-21 20:21:18 +0530

    Update snake_water_gun.py

--- snake_water_gun.py
+++ snake_water_gun.py
@@ -1,10 +1,10 @@
 import random
 print("key:- s:snake w:water g:gun")
-computer = random.choice([1,-1,0])
-youstr = input("chosse your key:")
+youstr = input("chosse your key:")  #Getting input from user.
+computer = random.choice([1,-1,0])  #Getting input from computer.
 youdisc = {"s":1,"w":-1,"g":0}
 reverse_disc = {1:"snake",-1:"water",0:"gun"}
-you = youdisc[youstr]
+you = youdisc[youstr]             
 print(f"\n you choosed {reverse_disc[you]} \n computer chossed {reverse_disc[computer]}")
 if(you == computer):
     print("Draw Match!")
